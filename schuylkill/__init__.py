f38196cad76b27475cc28e9be93c6aed73386c46
Nick Hand <nicholas.adam.hand@gmail.com>
2022-11-14 10:15:17 -0500

    update dependencies

--- schuylkill/__init__.py
+++ schuylkill/__init__.py
@@ -1,8 +1,4 @@
-try:
-    from importlib.metadata import version
-except ImportError:
-    # Running on pre-3.8 Python; use importlib-metadata package
-    from importlib_metadata import version
+from importlib.metadata import version
 
 __version__ = version(__package__)
 
