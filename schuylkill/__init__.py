17dcc2e3fcfd77f3d8d356361a6deda4e25ff73d
Nick Hand <nicholas.adam.hand@gmail.com>
2022-11-14 10:05:13 -0500

    add version

--- schuylkill/__init__.py
+++ schuylkill/__init__.py
@@ -4,6 +4,8 @@ except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     from importlib_metadata import version
 
+__version__ = version(__package__)
+
 from .exact import exact_merge
 from .fuzzy import fuzzy_merge
 from .tf_idf import tf_idf_merge
